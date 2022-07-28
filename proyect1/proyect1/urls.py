91690dff5df6e7dbd23c393b265c8dbc728640a9
leomatias <lmatias.mansilla@hotmail.com.ar>
2022-07-27 23:25:08 -0300

    se agrego una lista con for

--- proyect1/proyect1/urls.py
+++ proyect1/proyect1/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from proyect1.views import saludo, desafio
+from proyect1.views import saludo, desafio, template_con_lista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("saludo/", saludo, name="view_de_saludo"), 
-    path("desafio/", desafio, name="desafio" )
+    path("desafio/", desafio, name="desafio" ),
+    path("template_con_lista/", template_con_lista, name= "template_con_lista")
 ]
--- proyect1/proyect1/views.py
+++ proyect1/proyect1/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render
+from datetime import datetime
 
 
 def saludo(request):
     return HttpResponse("Hola Mundo!!")
 
 def desafio(request):
+    today = datetime.now()
     context = {
         'nombre' : 'juan',
-        'apellido' : 'Perez'
+        'apellido' : 'Perez',
+        'today' : today
     }
     return render(request, "desafio.html", context = context)
 
+def template_con_lista(request):
+    context = {
+        "lista" : ["tomate", "naranja", "banana"]
+    }
+    return render(request, "template_con_lista.html", context = context)
