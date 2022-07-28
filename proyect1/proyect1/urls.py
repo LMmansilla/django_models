3e7288cccfe220c08bc5e015923991b90e565182
leomatias <lmatias.mansilla@hotmail.com.ar>
2022-07-27 21:16:20 -0300

    desafio

--- proyect1/proyect1/urls.py
+++ proyect1/proyect1/urls.py
@@ -2,16 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 
-from proyect1.views import saludo, canelones, dia_de_hoy, saludonombre, anionacimiento, plantilla
+from proyect1.views import saludo, desafio
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("saludo/", saludo, name="view_de_saludo"), 
-    path("canelones/", canelones, name= "canelones"),
-    path("dia-de-hoy/", dia_de_hoy, name = "dia de hoy"),
-    path("saludo-con-nombre/<str:nombre>", saludonombre, name = "saludo con nombre"),
-    path("nacimiento/<int:edad>", anionacimiento, name= "nacimiento"),
-    path("plantilla/", plantilla, name="plantilla")
+    path("desafio/", desafio, name="desafio" )
 ]
--- proyect1/proyect1/views.py
+++ proyect1/proyect1/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponse
-from datetime import datetime
-
 
 from django.shortcuts import render
 
@@ -8,24 +6,6 @@ from django.shortcuts import render
 def saludo(request):
     return HttpResponse("Hola Mundo!!")
 
-def canelones(request):
-    return HttpResponse("Canelones de carne")
+def desafio(request):
+    return render(request, "desafio.html", context={})
 
-def dia_de_hoy(request):
-    hoy = datetime.today().date()
-    return HttpResponse(f"hoy es {hoy}")
-
-def saludonombre(request, nombre):
-    return HttpResponse(f"Hola amigo {nombre}")
-
-
-def anionacimiento(request, edad):
-    anioactual = datetime.today().year
-    nacimiento = anioactual - edad
-    return HttpResponse(f"Naciste en el {nacimiento}")
-
-def plantilla(request):
-    return render(request, "plantilla.html", context= {})
-
-
-    
